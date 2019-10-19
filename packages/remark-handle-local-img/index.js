2adace6eb962620205c8309897f26b9d80af52a6
zjf <zjffun@gmail.com>
2019-10-20 02:42:25 +0800

    add a remark handle local img plugin

---
+++ packages/remark-handle-local-img/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+const visit = require('unist-util-visit')
+const is = require('unist-util-is')
+
+const isImgExt = str => /\.(svg|png|jpg|jpeg|gif)$/.test(str)
+
+module.exports = function attacher() {
+    return function transformer(tree, file) {
+        visit(tree, visitor(file));
+    }
+}
+
+function visitor(file) {
+    return function (node) {
+        if (is(node, 'image') && isImgExt(node.url) && path.isAbsolute(node.url)) {
+            const extname = path.extname(node.url);
+            const dirPath = path.join(file.cwd, file.dirname);
+            const imgDirPath = path.join(dirPath, 'images');
+            const imgPath = path.join(imgDirPath, `${node.alt}${extname}`)
+            !fs.existsSync(imgDirPath) && fs.mkdirSync(imgDirPath);
+            var newImgPath = copyFileSyncWithKeepBoth(node.url, imgPath);
+            node.url = `./${path.relative(dirPath, newImgPath).replace(/\\/g, "/")}`;
+        }
+    }
+}
+
+function copyFileSyncWithKeepBoth(source, target) {
+    let newTarget = target;
+    let pathObj = null;
+    while (fs.existsSync(newTarget)) {
+        pathObj = path.parse(newTarget);
+        pathObj.base = `${pathObj.name}(z)${pathObj.ext}`
+        newTarget = path.format(pathObj);
+    }
+    fs.copyFileSync(source, newTarget);
+    return newTarget;
+}
